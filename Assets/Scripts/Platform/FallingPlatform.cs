d79bb94fb5239320d0dbe735c65b71cfc7955cf6
P KJ <pkjrockzz@gmail.com>
2023-07-25 11:18:04 +0530

    Fix Falling platform issue

--- Assets/Scripts/Platform/FallingPlatform.cs
+++ Assets/Scripts/Platform/FallingPlatform.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private float fallDelay = 1f;
-    [SerializeField] private float destroyDelay = 1f;
+    [SerializeField] private float respawnDelay = 1f;
 
     [SerializeField] private Rigidbody2D rb;
 
+    private Vector3 initialPosition;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -22,6 +28,9 @@ public class FallingPlatform : MonoBehaviour
     {
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, destroyDelay);
+        yield return new WaitForSeconds(respawnDelay);
+        rb.position = initialPosition;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = Vector3.zero;
     }
 }
